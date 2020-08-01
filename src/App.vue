4749cb72d567e37377348e078cec47473514cb5f
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-08-01 13:21:27 +0300

    Remove HelloWorld component and add WebMap component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <web-map />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WebMap from "./components/WebMap";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WebMap,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html,body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+    #app {
+      display: flex;
+      flex-direction: column;
+      padding: 0;
+      margin: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
 </style>
